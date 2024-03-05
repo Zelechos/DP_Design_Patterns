02b3bc17d33c5da5a9ef84acca1017a3538df451
Alexth <zelechos@hotmail.com>
2024-03-05 16:23:01 -0400

    style(v0.1.1): reformat code

--- design_patterns/abstract_factory/src/exercise_001/Application.java
+++ design_patterns/abstract_factory/src/exercise_001/Application.java
@@ -5,21 +5,21 @@ import exercise_001.abstract_products.IButton;
 import exercise_001.abstract_products.ICheckbox;
 
 public class Application {
-    private IGUIFactory factory;
+    private final IGUIFactory factory;
     private IButton button;
     private ICheckbox checkbox;
 
-    public Application(IGUIFactory factory){
+    public Application(IGUIFactory factory) {
         this.factory = factory;
     }
 
-    public void createUI(){
-       this.button = this.factory.createButton();
-       this.checkbox = this.factory.createCheckbox();
+    public void createUI() {
+        this.button = this.factory.createButton();
+        this.checkbox = this.factory.createCheckbox();
     }
 
-    public void paint(){
-       button.paint();
-       checkbox.paint();
+    public void paint() {
+        button.paint();
+        checkbox.paint();
     }
 }
